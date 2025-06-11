13c77490e47f4cc2ea775589aa3d0c28a56332a5
Khaled Hamada <khaledhamada779@gmail.com>
2025-06-11 21:27:50 +0300

    Finish RoomCard Heading Footer Home Page data and Room details page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +17,10 @@ export default function RootLayout({ children }) {
         className={inter.className}
       >
         <Header />
-        <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6"></main>
+        <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6">
           {children}
+        </main>
+        <Footer />
         </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import rooms from "@/data/rooms.json";
+import RoomCard from "@/components/RoomCard";
+import Heading from "@/components/Heading";
 
 export default function Home() {
   return (
     <>
-      <h1>Bookit App</h1>
+    <Heading title='Available Rooms' />
+      { rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} key={room.$id} /> )
+      ) : <p>No room available at the moment</p> }
     </>
   );
 }
